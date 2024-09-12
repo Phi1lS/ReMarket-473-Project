6237e7e2bbdcdb69b771c2598716f6758769a612
Phill Solis <psolis@emich.edu>
2024-09-12 01:24:46 -0400

    Fixed Android and Web Versions
    I fixed Android and Web Versions of the app by importing and using statusbar properly

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { StyleSheet, Platform, StatusBar, View } from 'react-native';
+import { StyleSheet, Platform, StatusBar, View } from 'react-native'; // Ensure StatusBar is imported from 'react-native'
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator } from '@react-navigation/stack'; 
 import { Ionicons } from '@expo/vector-icons';
 
 // Import screen components
@@ -12,10 +12,11 @@ import ShopPage from './pages/ShopPage';
 import SearchPage from './pages/SearchPage';
 import SellingPage from './pages/SellingPage';
 import ProfilePage from './pages/ProfilePage';
-import ItemDetailScreen from './pages/HomePages/ItemDetailScreen';
-import ItemPage from './pages/ItemPage';
+import ItemDetailScreen from './pages/HomePages/ItemDetailScreen'; 
+import ItemPage from './pages/ItemPage'; 
 import CreateListingPage from './pages/SellPages/CreateListingPage';
 import SearchUsersPage from './pages/HomePages/SearchUsersPage';
+import CartPage from './pages/ShopPages/CartPage';
 
 // Create Bottom Tab Navigator
 const Tab = createBottomTabNavigator();
@@ -32,10 +33,26 @@ function HomeStack() {
         headerTitleAlign: 'center',
       }}
     >
-      <Stack.Screen name="Home" component={HomePage} options={{ headerShown: false }} />
-      <Stack.Screen name="ItemDetail" component={ItemDetailScreen} options={{ title: 'Purchase' }} />
-      <Stack.Screen name="ItemPage" component={ItemPage} options={{ title: 'Item Details' }} />
-      <Stack.Screen name="SearchUsersPage" component={SearchUsersPage} options={{ title: 'Search Users' }} />
+      <Stack.Screen 
+        name="Home" 
+        component={HomePage} 
+        options={{ headerShown: false }} 
+      />
+      <Stack.Screen 
+        name="ItemDetail" 
+        component={ItemDetailScreen} 
+        options={{ title: 'Purchase' }} 
+      />
+      <Stack.Screen 
+        name="ItemPage" 
+        component={ItemPage} 
+        options={{ title: 'Item Details' }} 
+      />
+      <Stack.Screen 
+        name="SearchUsersPage" 
+        component={SearchUsersPage} 
+        options={{ title: 'Search Users' }} 
+      />
     </Stack.Navigator>
   );
 }
@@ -53,6 +70,7 @@ function ShopStack() {
     >
       <Stack.Screen name="Shop" component={ShopPage} options={{ headerShown: false }} />
       <Stack.Screen name="ItemPage" component={ItemPage} options={{ title: 'Item Details' }} />
+      <Stack.Screen name="CartPage" component={CartPage} options={{title: 'Your Cart' }} />
     </Stack.Navigator>
   );
 }
@@ -79,45 +97,47 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ color, size, focused }) => {
-              let iconName;
-              if (route.name === 'Home') {
-                iconName = 'home';
-              } else if (route.name === 'Shop') {
-                iconName = 'cart';
-              } else if (route.name === 'Search') {
-                iconName = 'search';
-              } else if (route.name === 'Selling') {
-                iconName = 'pricetag';
-              } else if (route.name === 'My Profile') {
-                iconName = 'person';
-              }
-              return <Ionicons name={iconName} size={focused ? size + 8 : size} color={color} />;
-            },
-            tabBarActiveTintColor: '#58A4B0',
-            tabBarInactiveTintColor: 'gray',
-            tabBarStyle: {
-              height: Platform.OS === 'ios' ? 90 : 70, // Adjust height for Android and iOS
-              paddingBottom: Platform.OS === 'ios' ? 30 : 15, // Add padding for iOS/Android
-              paddingTop: 10, // Padding at the top for both platforms
-              shadowColor: '#000',
-              shadowOffset: { width: 0, height: -2 },
-              shadowOpacity: 0.1,
-              shadowRadius: 6,
-              elevation: 5,
-            },
-            headerShown: false,
-            tabBarAnimationEnabled: true,
-          })}
-        >
-          <Tab.Screen name="Home" component={HomeStack} />
-          <Tab.Screen name="Shop" component={ShopStack} />
-          <Tab.Screen name="Search" component={SearchPage} />
-          <Tab.Screen name="Selling" component={SellingStack} />
-          <Tab.Screen name="My Profile" component={ProfilePage} />
-        </Tab.Navigator>
+        <View style={styles.container}>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size, focused }) => {
+                let iconName;
+                if (route.name === 'Home') {
+                  iconName = 'home';
+                } else if (route.name === 'Shop') {
+                  iconName = 'cart';
+                } else if (route.name === 'Search') {
+                  iconName = 'search';
+                } else if (route.name === 'Selling') {
+                  iconName = 'pricetag';
+                } else if (route.name === 'My Profile') {
+                  iconName = 'person';
+                }
+                return <Ionicons name={iconName} size={focused ? size + 8 : size} color={color} />;
+              },
+              tabBarActiveTintColor: '#58A4B0',
+              tabBarInactiveTintColor: 'gray',
+              tabBarStyle: {
+                height: Platform.OS === 'ios' ? 90 : 70,
+                paddingBottom: Platform.OS === 'ios' ? 30 : 15,
+                paddingTop: 10,
+                shadowColor: '#000',
+                shadowOffset: { width: 0, height: -2 },
+                shadowOpacity: 0.1,
+                shadowRadius: 6,
+                elevation: 5,
+              },
+              headerShown: false,
+              tabBarAnimationEnabled: true,
+            })}
+          >
+            <Tab.Screen name="Home" component={HomeStack} />
+            <Tab.Screen name="Shop" component={ShopStack} />
+            <Tab.Screen name="Search" component={SearchPage} />
+            <Tab.Screen name="Selling" component={SellingStack} />
+            <Tab.Screen name="My Profile" component={ProfilePage} />
+          </Tab.Navigator>
+        </View>
       </NavigationContainer>
     </PaperProvider>
   );
@@ -127,6 +147,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, // Ensure the padding is only for Android
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, // Ensure padding is only for Android
   },
 });
--- pages/SellPages/CreateListingPage.js
+++ pages/SellPages/CreateListingPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image, ScrollView, Platform } from 'react-native';
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, Image, ScrollView, StatusBar, Platform } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
